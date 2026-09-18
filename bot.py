c5508d77e24d050f1bb5dfae520fdfd4112216f1
z2585y8btd-stack <z2585y8btd@privaterelay.appleid.com>
2026-09-18 13:14:59 +0300

    Update /start with three inline buttons

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🔥 Join Private Channel", url="https://t.me/+LIVzUK7_TxphNGZk")]
+        [InlineKeyboardButton("🔥  Channel   🔥", url="https://t.me/+LIVzUK7_TxphNGZk")],
+        [InlineKeyboardButton("👻 Snapchat - 1500 Stars", url="https://snapchat.com/add/Sela.mon")],
+        [InlineKeyboardButton("💬  Telegram Account - 1000 Stars", url="https://t.me/Sullmh")]
     ])
     await update.message.reply_text("", reply_markup=keyboard)
 
