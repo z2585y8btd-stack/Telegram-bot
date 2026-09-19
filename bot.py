60a57e919011a0a478bcb43e5924bf2d493cc459
z2585y8btd-stack <z2585y8btd@privaterelay.appleid.com>
2026-09-19 12:00:43 +0300

    Set default Telegram owner ID for /setmusic

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from telegram.ext import (
 )
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-OWNER_ID = int(os.environ["TELEGRAM_OWNER_ID"])
+OWNER_ID = int(os.environ.get("TELEGRAM_OWNER_ID", "8561249287"))
 MUSIC_FILE = Path(os.environ.get("MUSIC_FILE", "music_file_id.txt"))
 
 
