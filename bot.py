7b3f3559f9a534b791a201ed8313afeac16a3994
z2585y8btd-stack <z2585y8btd@privaterelay.appleid.com>
2026-09-19 16:25:31 +0300

    Change reply text to include a mailbox emoji
    Updated the reply text to include an emoji.

--- bot.py
+++ bot.py
@@ -97,7 +97,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
     await update.message.reply_text(
-        "main branch.📩 اترك رسالتك",
+        "📥",
         reply_markup=reply_keyboard,
     )
 
